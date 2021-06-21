4aed63aadd038401d99236793d95c47434fffc44
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2021-06-21 16:24:39 +0200

    Update notnoel.js

--- notnoel.js
+++ notnoel.js
@@ -1,5 +1,5 @@
 function notnoel(console, options, notnoelclient) {
   console.log('Test!')
-  this.noel = require('./connecting_stuff/noelconnect.js')
+  this.noel = require('./noelconnect.js')
   this.noel2 = require('')
 }
